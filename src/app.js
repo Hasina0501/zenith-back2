1edf61da542e440ea133a346f4708c1dde1c0cb7
maxjohn81 <rivoandriharisoa@gmail.com>
2026-03-04 17:18:44 +0300

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
-const cors =require('cors')
+const cors = require('cors')
 const express = require(`express`)
 const adminRoutes = require('./routes/admin.route')
 
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/admin', adminRoutes)
